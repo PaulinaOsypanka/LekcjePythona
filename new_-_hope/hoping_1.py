e60ba337e631e2d037e86cc835c9be9c90181fde
PaulinaOsypanka <paulina.osypanka@gmail.com>
2020-10-03 09:37:01 +0200

    njuhopelekcjyaja8

---
+++ new_-_hope/hoping_1.py
@@ -0,0 +1,22 @@
+print(4*6)
+print("jaś jest głupi")
+print('oh shit, here we go again')
+print("it\'s a \\\\ rainy day")
+
+
+
+dniwtyg = 7
+minwh = 60
+hwdniu = 24
+h25wdniu = 25
+
+doba25tyg = dniwtyg*minwh*h25wdniu
+dobatyg = dniwtyg*minwh*hwdniu
+
+
+print(dniwtyg*minwh*hwdniu)
+print(dniwtyg*minwh*h25wdniu)
+print("wynik:")
+print(doba25tyg-dobatyg)
+
+print("Gdyby doba miałaby 25h zyskalibyśmy 420 minut tygodnjowo")
