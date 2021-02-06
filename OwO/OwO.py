81bca49cc45f60a2394f45d3d406978e5c07b09c
Paulina Osypanka <paulina.osypanka@gmail.com>
2021-02-06 10:30:46 +0000

    kommit lol

---
+++ OwO/OwO.py
@@ -0,0 +1,36 @@
+class szpulka():
+    def pomalujcieszpilkianiijejkuzynkinaczarnobojestmarno(self):
+        self.color = 'czarny'
+    
+    def pomalujismarujmaslobialemaslo(self):
+        self.color = 'biały'
+
+    def supernova(self,nowykolor):
+        self.color = nowykolor
+
+    def __init__(self,nofykolor):
+        self.color = nofykolor
+
+    def __str__(self):
+        return "jestem piułka pigułka ja ha ja ja jaaaa"
+
+# koszowapila = szpulka()
+# koszowapila.pomalujcieszpilkianiijejkuzynkinaczarnobojestmarno()
+# print(koszowapila.color)
+
+# koszowapila.pomalujismarujmaslobialemaslo()
+# print(koszowapila.color)
+
+# pnika = szpulka()
+# pnika.pomalujcieszpilkianiijejkuzynkinaczarnobojestmarno()
+# print(pnika.color)
+# print(koszowapila.color)
+
+# pilkalekufalenieletkatylkolecznicza = szpulka()
+# pilkalekufalenieletkatylkolecznicza.supernova('amelinowy+')
+# print(pilkalekufalenieletkatylkolecznicza.color)
+
+rukpytomukpyurukfyiniemamukwy = szpulka('sraczkoburaczkowy to kolor twoich majtek lol')
+print(rukpytomukpyurukfyiniemamukwy.color)
+
+print(rukpytomukpyurukfyiniemamukwy)
--- new_-_hope/OwO.py
+++
@@ -1,5 +0,0 @@
-from time import sleep
-
-for i in range(50): 
-    print('OwO')
-    sleep(0.6)
